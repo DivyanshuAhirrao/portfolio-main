a60acbd11d5d889f3cb2f72a67017ceb93404bc9
DivyanshuAhirrao <246divyanahuahirrao@gmail.com>
2024-08-18 18:55:14 +0530

    code optimizatiom - Project

---
+++ src/components/projects/ViewButton.jsx
@@ -0,0 +1,9 @@
+const ViewButton = () => {
+  return (
+    <button className="relative top-2 bg-purple-400 px-2 rounded-md py-[2px] cursor-pointer hover:outline-double hover:border-purple-700 hover:bg-gray-100 transition-all duration-200 lg:text-[1.7vw] xs:text-[16px]">
+      View Demo
+    </button>
+  );
+};
+
+export default ViewButton;
