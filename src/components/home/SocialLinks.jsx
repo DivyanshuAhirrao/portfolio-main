49170270864232cd5e50a4c919712213b5aec7a2
Divyanshu Ahirrao <246divyanahuahirrao@gmail.com>
2025-09-02 23:43:38 +0530

    Resume dynamic storage link implemented with vercel storage as blob.

--- src/components/home/SocialLinks.jsx
+++ src/components/home/SocialLinks.jsx
@@ -3,35 +3,35 @@ import { FaInstagram } from "react-icons/fa";
 import { ImWhatsapp } from "react-icons/im";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import cv from "../media/Divyanshu_CV.pdf";
 const SocialLinks = () => {
   const handleClick = () => {
     const whatsappUrl = `https://wa.me/7972814411`;
     window.open(whatsappUrl, "_blank");
   };
   const socialData = [
-    { href: "#", icon: FaFacebook, hoverClass: "hover:text-blue-600" },
+    { href: "#", icon: FaFacebook, hoverClass: "blue-600" },
     {
       href: "https://www.instagram.com/iam.divyanshu?igsh=MWZIZDc2N2hmZXRyYw==",
       icon: FaInstagram,
-      hoverClass: "hover:text-pink-700",
+      hoverClass: "pink-700",
     },
     {
       href: "https://www.linkedin.com/in/divyanshu-ahirrao-038537245/",
       icon: FaLinkedin,
-      hoverClass: "hover:text-purple-700",
+      hoverClass: "purple-700",
     },
     {
       onClick: handleClick,
       icon: ImWhatsapp,
-      hoverClass: "hover:text-green-500",
+      hoverClass: "green-500",
     },
     {
       href: "https://github.com/DivyanshuAhirrao",
       icon: FaGithub,
-      hoverClass: "hover:text-orange-700",
+      hoverClass: "orange-700",
     },
   ];
+  const resumeVercelLink = 'https://ua30zxjphjwjhfdk.public.blob.vercel-storage.com/divyanshu_swe_resume.pdf';
   return (
     <>
       <section className="pl-5 xs:pt-4 sm:pt-8">
@@ -41,7 +41,7 @@ const SocialLinks = () => {
               key={index}
               href={link.href || undefined}
               onClick={link.onClick || undefined}
-              className={`${link.hoverClass} cursor-pointer transition-all duration-200`}
+              className={`hover:text-${link.hoverClass} cursor-pointer transition-all duration-200`}
             >
               <link.icon />
             </a>
@@ -55,7 +55,7 @@ const SocialLinks = () => {
             </button>
           </a>
 
-          <a href={cv} download>
+          <a href={resumeVercelLink} download target="_blank">
             <button className="py-[7.2px] px-6 border-2 border-gray-800 hover:bg-gray-700 transition-all duration-200 hover:text-gray-200 text-gray-700 rounded-[5px]">
               DOWNLOAD CV
             </button>
