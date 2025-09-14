8654bc2fbbb3ed478aa8284b51d41ae855037b67
Divyanshu Ahirrao <246divyanahuahirrao@gmail.com>
2025-09-14 16:43:44 +0530

    Optmized Performance Enhancement 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Layout from "./components/Layout";
+import { lazy, Suspense } from "react";
 import ScrollBackgroundAnimation from "./components/animation/BackgroundAnimation";
 import ParallaxWrapper from "./components/animation/ParallexWrapper";
 import ScrollProgressBar from "./components/animation/ScrollProgressBar"; // NEW IMPORT
 import TrainAnimation from "./components/animation/TrainAnimation";
 import { ThemeProvider } from "./components/context/ThemeContext";
+const Layout = lazy(() => import("./components/Layout"));
 
 const App = () => {
   return (
@@ -11,9 +12,11 @@ const App = () => {
       <ScrollBackgroundAnimation />
       <ScrollProgressBar />
        <TrainAnimation /> 
+       <Suspense fallback={<div className="text-center py-10">Loading…</div>}>
       <ParallaxWrapper> {/* NEW WRAPPER */}
         <Layout />
       </ParallaxWrapper>
+      </Suspense>
     </ThemeProvider>
   );
 };
